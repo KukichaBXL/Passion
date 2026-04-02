6f8b11dafb32286f9134343a3efe03e5441f1641
KukichaBXL <craeyerobin@gmail.com>
2026-04-02 11:16:27 +0200

    Create include

---
+++ view/inc/footer.php
@@ -0,0 +1,3 @@
+<div>
+        <p>&copy; 2026 Craeye Robin - Projet de formation Web Dev Passion</p>
+      </div>
---
+++ view/inc/menu.php
@@ -0,0 +1,11 @@
+<nav>
+      <div class="contenu">
+        <a href=""><h1>L'art de la peinture</h1></a>
+        <ul>
+          <li><a href="./">Accueil</a></li>
+          <li><a href="./?p=histoire">Histoire</a></li>
+          <li><a href="./?p=techniques">Techniques</a></li>
+          <li><a href="./?p=galerie">Galerie</a></li>
+        </ul>
+      </div>
+    </nav>
