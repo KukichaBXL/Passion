69bc6d5643af20289b1906509e68315c7a99d70e
KukichaBXL <craeyerobin@gmail.com>
2026-04-02 11:15:43 +0200

    Create 404 page

---
+++ view/404.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="FR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Passion : La peinture</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <!-- NAV -->
+
+    <?php include "inc/menu.php" ?>
+
+    
+
+    <!-- HEADER -->
+
+    <section class="header">
+      <h2>ERREUR 404</h2>
+      <p id="citation">
+        "Bien essayé" <br />
+        - "Vincent Van Gogh"
+      </p>
+    </section>
+
+    <!-- CONTENU -->
+    <section class="intro">
+      <div>
+        <h3>Erreur 404</h3>
+        <p>
+          Il semble que nous avons rencontré un petit problème. Veuillez retournez sur l'accueil.
+        </p>
+      </div>
+    </section>
+
+    <!-- FOOTER -->
+
+    <footer>
+      <?php include "inc/footer.php"; ?>
+    </footer>
+  </body>
+</html>
